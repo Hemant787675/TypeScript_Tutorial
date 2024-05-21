001bbe738e84d2d24619eb5572747132064da575
Hemant787675 <161458568+Hemant787675@users.noreply.github.com>
2024-05-21 20:31:23 +0530

    app.js
    Compiled  TypeScript file into JavaScript File

---
+++ 7.tsconfig.json-File/public/app.js
@@ -0,0 +1,11 @@
+"use strict";
+console.log('HY');
+//In this lesson we will learn how we can use TypeScript in a better way:
+//Primarily we need to create two folders: public- It contains static files or compiled JS file and another one is src- It contains TypeScript file
+//Afterward we will initialize TypeScript with this command: tsc --init (We need to run this command in a folder that contains public and src folder)
+//A tsconfig.json file will be generated
+//We need to uncomment and write ./src on rootDir and ./public in ourDir as i have written in ts.config file
+//Afterward craete a file in src folder with .ts extension and run the follwing command in after opening terminal: 
+//tsc
+//It will comile the typescript file and generate a JS file in public folder 
+//If we have written some code in TypeScript that will be compiled in JS file and we can that JS file code with this command after going in public command in terminal: node <JS filename>.js
