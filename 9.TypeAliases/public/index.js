2338ca82b4db677dace7ca2bd700a5b0c5bf91e6
Hemant787675 <161458568+Hemant787675@users.noreply.github.com>
2024-05-22 11:46:14 +0530

    Compiled JavaScript File

---
+++ 9.TypeAliases/public/index.js
@@ -0,0 +1,8 @@
+var details = function (emp) {
+    console.log("Hy ".concat(emp.Name, ", your id is ").concat(emp.Id));
+};
+var empDetails = {
+    Name: "Ajay",
+    Id: 420
+};
+details(empDetails);
