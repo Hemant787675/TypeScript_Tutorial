c2bf13c9219c5a8bf2bb353cc2ed4f1523685b2d
Hemant787675 <161458568+Hemant787675@users.noreply.github.com>
2024-05-25 18:52:34 +0530

    index.js

---
+++ 13.Modules in Typescript/public/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const dept_js_1 = require("./module/dept.js");
+const obj = new dept_js_1.dept('Sales', 'Ravi Kant, Sushmita Sen', 3);
+console.log(obj);
