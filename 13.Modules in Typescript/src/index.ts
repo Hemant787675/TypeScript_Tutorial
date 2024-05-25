691a6ac0c96cf1df5f79a6d3746ecc012bc3bdeb
Hemant787675 <161458568+Hemant787675@users.noreply.github.com>
2024-05-25 19:03:20 +0530

    index.ts

---
+++ 13.Modules in Typescript/src/index.ts
@@ -0,0 +1,7 @@
+//Here we are importing the dept module
+import {dept} from './module/dept.js';
+
+//Here we are creating an object 
+const obj= new dept('Sales','Ravi Kant, Sushmita Sen',3);
+
+console.log(obj);
