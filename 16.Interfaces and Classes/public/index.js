dd8771189ce60fdba91e6be579249e3f67ad2b12
Hemant <161458568+Hemant787675@users.noreply.github.com>
2024-05-28 19:29:00 +0530

    index.js

---
+++ 16.Interfaces and Classes/public/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const emp_js_1 = require("./classes/emp.js");
+const obj2 = new emp_js_1.emp('Ajay', 9000);
+console.log(obj2.res());
